42ecf26ea7928ea074892de10b6a84d609c6d8cf
Stefano Faraggi <40745694+stepp1@users.noreply.github.com>
2025-09-15 19:34:08 +0200

    Add 'estimate' to module exports

--- bm/__init__.py
+++ bm/__init__.py
@@ -1,9 +1,10 @@
 """Synthetic distribution module for the bm package."""
 
+from bm import estimate
 from bm.estimate import estimate_synthetic_mi
 from bm.synthetic_distribution import SyntheticDistribution
 
 # Don't import run here to avoid circular imports when run.py is executed directly
-__all__ = ["SyntheticDistribution", "estimate_synthetic_mi"]
+__all__ = ["SyntheticDistribution", "estimate", "estimate_synthetic_mi"]
 
 __version__ = "0.1.0"
